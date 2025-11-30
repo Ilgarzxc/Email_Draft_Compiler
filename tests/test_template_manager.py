05b1d257827952b16448e5f7e4443c8614ed2be7
Ilgar Gurbanov <ilgar.gurbanov.90@gmail.com>
2025-11-30 19:24:35 +0200

    Added main template. Commented out unused parts of template manager document

--- tests/test_template_manager.py
+++ tests/test_template_manager.py
@@ -3,16 +3,23 @@ from prettytable import PrettyTable
 
 ''' 
 Required placeholders in the template:
-- Sender email address (required validation: if no spaces and "@" sign present)
-- Receiver email address (required validation: if no spaces and "@" sign present)
-- Receiver first name (no digits allowed, first letter capitalized)
-- User login (validation: at least 5 characters, no spaces)
-- Environment name (from the environments_placeholders dictionary)
-- Link to the environment (from the environments_placeholders dictionary)
+- Sender email address (required validation: if no spaces and "@" sign present). Variable name: my_mailbox
+- Receiver email address (required validation: if no spaces and "@" sign present). Variable name: recepient_email
+- Receiver first name (no digits allowed, first letter capitalized). Variable name: recepient_name
+- User login (validation: at least 5 characters, no spaces). Variable name: user_login
+- Environment name (from the environments_placeholders dictionary). Variable name: environment_name
+- Link to the environment (from the environments_placeholders dictionary). Variable name: environment_link
+- Signature - predefined text block (no validation). Variable name: signature
 
 All fields must be filled before sending the email and mandatory.
 '''
 
+spreadsheet_from_template = PrettyTable()
+
+spreadsheet_from_template.field_names = ["environment name", "link to the environment"]
+spreadsheet_from_template.add_row(["login", "password"])
+
+print(spreadsheet_from_template)
 
 # Environment and links dictionary
 environments_placeholders = {
@@ -23,18 +30,13 @@ environments_placeholders = {
 
 # Email template
 
-Dear {recepient_name},
+'''Dear {recepient_name},
 
 Your account has been created. 
-
-spreadsheet_from_template = PrettyTable()
-
-spreadsheet_from_template.field_names = ["environment name", "link to the environment"]
-spreadsheet_from_template.add_row(["login", "password"])
 
 Password sent in a separate email. Please check your mailbox.
 
 If you experience issues with your account, please contact us at {mailbox} for further assistance. 
 
 Best regards,
-{signature}
+{signature}'''
