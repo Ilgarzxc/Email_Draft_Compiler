f6a0c660865abb72629bba404bc27a78f0c676a6
Ilgar Gurbanov <ilgar.gurbanov.90@gmail.com>
2025-12-04 19:43:51 +0200

    Added functions process_user_input and assemble_email to the template_manager.py

--- src/template_manager.py
+++ src/template_manager.py
@@ -1,27 +1,38 @@
-# importing PrettyTable for future use
-from prettytable import PrettyTable 
-
-# Environment and links dictionary
-environments_placeholders = {
-    "subscription module": "http://dev.example.com",
-    "aci prod": "http://staging.example.com",
-    "aci test": "http://prod.example.com"
-}
-
-# Email template
-
+def assemble_email(recipient_name: str, username: str, user_login: str, signature:str) -> str:
+    email_text = f"""
 Dear {recepient_name},
 
-Your account has been created. 
+Your account in {environment_name} has been created. 
 
-spreadsheet_from_template = PrettyTable()
-
-spreadsheet_from_template.field_names = ["environment name", "link to the environment"]
-spreadsheet_from_template.add_row(["login", "password"])
++------------------------+-------------------------------------+
+| {environment_name}     | {environment_link}                  |
++------------------------+-------------------------------------+
+| {username}             |  {user_login}                       |
++------------------------+-------------------------------------+
 
 Password sent in a separate email. Please check your mailbox.
 
-If you experience issues with your account, please contact us at {mailbox} for further assistance. 
+If you experience issues with your account, please contact us at ilgar.gurbanov.90@gmail.com for further assistance. 
 
 Best regards,
 {signature}
+"""
+    return email_text.strip()
+
+
+
+
+def process_user_input(recipient_name: str, username: str, user_login: str, signature: str) -> str:
+
+    validate_recipient_name(recipient_name)
+    validate_username(username)
+    validate_user_login(user_login)
+    validate_signature(signature)
+
+    return assemble_email(
+        recipient_name=recipient_name,
+        username=username,
+        user_login=user_login,
+        signature=signature
+    )
+
